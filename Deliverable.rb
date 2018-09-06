4b15179b2b687e56482256a259aa9568a4c0d783
cpkPitt <43021691+cpkPitt@users.noreply.github.com>
2018-09-05 22:48:13 -0400

    Add files via upload

--- Deliverable.rb
+++ Deliverable.rb
@@ -1,16 +1,22 @@
-puts "This game of Super Connect-Four will consist of #ARGV[0] rows and columns"
+color 02
+puts "\nThis game of Super Connect-Four will consist of #{ARGV[0]} rows and columns"
 
-puts "Here is the current game board:"
+puts "\nHere is the current game board:\n "
 
 
 #Still need input validation loop here 
 v1 = ARGV[0].to_i
 
 
-
-(0..v1).each do |i|
-	(0..v1-1).each do |j|
-		print "."
+def draw(v1)
+	(0..v1).each do |i|
+		(0..v1-1).each do |j|
+			print "."
+		end
+		puts "."
 	end
-	puts "."
+	print "\n"
 end
+
+draw(v1)
+
